4d77f5ecf57b0a37ec5e24651ddb2fe6089fae61
Rahul Chauhan <rahulchauhan9292@gmail.com>
2020-11-18 23:31:56 +0530

    Initial Commit

---
+++ src/main/java/com/aws/AwsLambdaApp.java
@@ -0,0 +1,28 @@
+package com.aws;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class AwsLambdaApp implements RequestHandler<AwsLambdaRequest, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(AwsLambdaRequest awsLambdaRequest, Context context) {
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        try {
+            int day = awsLambdaRequest.getDay();
+            int month = awsLambdaRequest.getMonth();
+            int year = awsLambdaRequest.getYear();
+            LocalDate l = LocalDate.of(year, month, day);
+            LocalDate now = LocalDate.now();
+            Period diff = Period.between(l, now);
+            String output = String.format(diff.getYears() + " years " + diff.getMonths() + " months " + diff.getDays() + " days");
+            return response.withBody(output);
+        } catch (Exception e) {
+            return response.withBody("{}").withStatusCode(500);
+        }
+    }
+}
